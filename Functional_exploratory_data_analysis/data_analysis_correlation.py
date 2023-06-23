d15a58f4d6962ef02cf572630c4f17b2fc2b2d17
Yaser Atalay <119959706+YaserAtalay@users.noreply.github.com>
2023-06-24 00:09:45 +0300

    Add files via upload

---
+++ Functional_exploratory_data_analysis/data_analysis_correlation.py
@@ -0,0 +1,28 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import  pandas as pd
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 500)
+df = sns.load_dataset("tips")
+
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(bool))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show(block=True)
+    return drop_list
+
+
+high_correlated_cols(df)
+drop_list = high_correlated_cols(df, plot=True)
+df.drop(drop_list, axis=1)
+high_correlated_cols(df.drop(drop_list, axis=1), plot=True)
